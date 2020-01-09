210f8d24ba3acbb943edd5cceac75a9cb90cf343
tangxqa <tangxq@haier.com>
2020-01-09 15:35:13 +0800

    bugfix: system failure when eureka server reboot

--- eureka/put.go
+++ eureka/put.go
@@ -1,8 +1,6 @@
 package eureka
 
 import (
-	asynclog "github.com/alecthomas/log4go"
-	"github.com/tangxqa/gogate/discovery"
 	"net/http"
 	"strings"
 )
@@ -18,10 +16,6 @@ func (c *Client) SendHeartbeat(appId, instanceId string) error {
 	case http.StatusNotFound:
 		return newError(ErrCodeInstanceNotFound,
 			"Instance resource not found when sending heartbeat", 0)
-	case http.StatusBadGateway:
-		asynclog.Info("receive 502, send regist instance info...!")
-		discovery.SendRegistInstanceInfo()
-
 	}
 	return nil
 }
